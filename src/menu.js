72a82cf1700ee66c5e2994327479784eab00268f
Lena <Hovhannisyan.Lena17@gmail.com>
2018-07-11 04:25:16 -0700

    [CG-1] Create Home page

---
+++ src/menu.js
@@ -0,0 +1,29 @@
+import Home from "./pages/home/Home";
+
+export default [
+  {
+    name: "Home",
+    link: "/",
+    component: Home
+  },
+  {
+    name: "Login",
+    link: "/login",
+    component: ""
+  },
+  {
+    name: "Pricing",
+    link: "/#pricing",
+    component: ""
+  },
+  {
+    name: "Features",
+    link: "/#features",
+    component: "Features"
+  },
+  {
+    name: "Gallery",
+    link: "/gallery",
+    component: "Gallery"
+  }
+];
